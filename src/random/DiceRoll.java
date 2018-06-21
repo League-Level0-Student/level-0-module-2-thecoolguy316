22a0d14b0b411a3760724af801dcc4ec6beadd63
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-21 13:26:00 -0700

    DiceRoll, IntroToRandom,RockPapercissors

--- src/random/DiceRoll.java
+++ src/random/DiceRoll.java
@@ -4,6 +4,8 @@
 package random;
 
 import java.awt.Dimension;
+import java.util.Random;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -29,7 +31,7 @@ public class DiceRoll {
 	public static void main(String[] args) {
 		new DiceRoll().run();
 	}
-	
+
 	public void run() {
 		try {
 			oneImg = new ImageIcon(getClass().getResource("dice 1.png"));
@@ -48,20 +50,21 @@ public class DiceRoll {
 		rollButton.addActionListener((e) -> {
 
 			// 1. Make randomChoice equal to a random number between 1 and 6
-			int randomChoice = 0;
+			Random f = new Random();
+					int randomChoice = f.nextInt(7);
 
 			// 2. Fix the code below so that it displays the correct image
-			if (randomChoice == 0) {
+			if (randomChoice == 1 ) {
 				label.setIcon(oneImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 2) {
 				label.setIcon(twoImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 3) {
 				label.setIcon(threeImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 4) {
 				label.setIcon(fourImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 5) {
 				label.setIcon(fiveImg);
-			} else {
+			} else if (randomChoice == 6) {
 				label.setIcon(sixImg);
 			}
 		});
@@ -74,5 +77,5 @@ public class DiceRoll {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
 	}
-
+	
 }
--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -6,6 +6,7 @@ package random;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -81,7 +82,9 @@ public class RockPaperScissors extends JPanel implements ActionListener{
         //2. Run the program 4 times. Does the computer always choose the same thing?
 
         //3. Change the value of opponentSelection to be a random number between 0 and 2;
-        int opponentSelection = 0;
+        Random F = new Random();
+        
+    	int opponentSelection = F.nextInt(3);
         
         //4. Run the program again. Is the result different?
  
